048f2200d1ac6dd0e1392095f5ce0e2b9cafc0ef
jockdeveloper <jockdeveloper@gmail.com>
2022-11-01 13:27:29 +0100

    Realizando Login y creando Token

---
+++ server/controllers/user.js
@@ -0,0 +1,61 @@
+const User = require("../models/user");
+const bcryptjs = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+function createToken(user, SECRET_KEY, expiresIn) {
+    const { id, name, email, username } = user;
+    const payload = {
+        id,
+        name,
+        email,
+        username,
+    };
+    return jwt.sign(payload, SECRET_KEY, { expiresIn });
+};
+
+async function register(input) {
+    const newUser = input;
+    newUser.email = newUser.email.toLowerCase();
+    newUser.username = newUser.email.toLowerCase();
+
+    const { email, username, password } = newUser;
+
+    // Revisamos si el email ya está en uso
+    const foundEmail = await User.findOne({ email });
+    if (foundEmail) throw new Error("El email ya está en uso");
+
+    // Revisamos si el username ya está en uso
+    const foundUsername = await User.findOne({ username });
+    if (foundUsername) throw new Error("El nombre de usuario ya está en uso");
+
+    // Encriptar password
+    const salt = await bcryptjs.genSaltSync(10);
+    newUser.password = await bcryptjs.hash(password, salt);
+
+    try {
+        const user = new User(newUser);
+        user.save();
+        return user;
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+async function login(input) {
+    const { email, password } = input;
+
+    const userFound = await User.findOne({ email: email.toLowerCase() });
+    if (!userFound) throw new Error("Error en el email o contraseña");
+
+    const passwordSuccess = await bcryptjs.compare(password, userFound.password);
+    if (!passwordSuccess) throw new Error("Error en el email o contraseña");
+
+    return {
+        token: createToken(userFound, process.env.SECRET_KEY, "24h"),
+    }
+}
+
+module.exports = {
+    register,
+    login,
+};
--- server/gql/resolver.js
+++ server/gql/resolver.js
@@ -1,5 +1,4 @@
-const User = require("../models/user");
-const bcryptjs = require("bcryptjs");
+const userController = require("../controllers/user");
 
 const resolvers = {
     Query: {
@@ -11,35 +10,8 @@ const resolvers = {
     },
     Mutation: {
         // User
-        register: async (_, { input }) => {
-            const newUser = input;
-            newUser.email = newUser.email.toLowerCase();
-            newUser.username = newUser.email.toLowerCase();
-
-            const { email, username, password } = newUser;
-
-            // Revisamos si el email ya está en uso
-            const foundEmail = await User.findOne({ email });
-            if (foundEmail) throw new Error("El email ya está en uso");
-
-            // Revisamos si el username ya está en uso
-            const foundUsername = await User.findOne({ username });
-            if (foundUsername) throw new Error("El nombre de usuario ya está en uso");
-
-            // Encriptar password
-            const salt = await bcryptjs.genSaltSync(10);
-            newUser.password = await bcryptjs.hash(password, salt);
-
-            try {
-                const user = new User(newUser);
-                user.save();
-                return user;
-            } catch (error) {
-                console.log(error);
-            }
-
-            return input;
-        }
+        register: (_, { input }) => userController.register(input),
+        login: (_, { input }) => userController.login(input),
     },
 }
 
